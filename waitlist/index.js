a611129d6e4f72885b67691574436b74a32b5cc0
MyrPasko <myroslavpasko@gmail.com>
2023-05-19 09:33:23 +0300

    Initial commit

---
+++ waitlist/index.js
@@ -0,0 +1,40 @@
+const awsServerlessExpress = require('aws-serverless-express');
+const app = require('./app');
+
+/**
+ * @type {import('http').Server}
+ */
+const server = awsServerlessExpress.createServer(app);
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event, context) => {
+  try {
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+
+    // Regenerate the previous request
+    const requestData = {
+      body: event.body,
+      headers: event.headers,
+      httpMethod: event.httpMethod,
+      isBase64Encoded: event.isBase64Encoded,
+      path: event.path,
+      pathParameters: event.pathParameters,
+      queryStringParameters: event.queryStringParameters,
+      requestContext: event.requestContext,
+      resource: event.resource,
+      stageVariables: event.stageVariables
+    };
+
+    return await awsServerlessExpress.proxy(server, requestData, context, 'PROMISE').promise;
+  } catch (error) {
+    console.error(error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error: ' + error.message })
+    };
+  }
+};
+
